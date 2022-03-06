2d7ca9d04513aa8c543a453b0c679040d26bdc5c
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-06 14:35:46 +0100

    adding km and discount to the layout

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log('JS OK!');
 
 // Variables
 const km = prompt('How many km would you like to travel?');
+document.getElementById("km").innerHTML = 'Your trip: ' + km + ' km';
 
 const age = prompt('How old are you?');
 
@@ -14,11 +15,13 @@ if (age < 18) {
     // under 18
     let total = (price - (20 * price / 100)).toFixed(2);
     document.getElementById("total").innerHTML = 'Final price: ' + total + '€';
+    document.getElementById("discount").innerHTML = 'Discount: 20%';
 
 } else if (age > 65) {
     // over 65
     let total = (price - (40 * price / 100)).toFixed(2);
     document.getElementById("total").innerHTML = 'Final price: ' + total + '€';
+    document.getElementById("discount").innerHTML = 'Discount: 40%';
 
 } else {
     // standard price
